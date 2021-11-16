0fdd3cd4e6b2f859f1cfeb6ace4bfd2dcc135d81
Seracrooce <fschmidt@live.com>
2021-11-16 15:11:27 -0600

    Did all the stuff from explorer 3. Stored old html in its own file for when I learn how to just comment it out. Used customized html in fishlistjs

--- scripts/database.js
+++ scripts/database.js
@@ -2,44 +2,61 @@ const database = {
   fish: [
     {
       id: 1,
+      image: "images/fish.jpg",
+      altText: "A dark purple fish with a red fin",
       name: "Bart",
       species: "Fishicus Maximus",
       food: "crustaceans",
       size: 3,
       location: "Mississippi River",
+      careTip1: "Don't let it die.",
+      careTip2: "It likes water",
     },
 
     {
       id: 2,
+      image: "images/fish.jpg",
+      altText: "A dark purple fish with a red fin",
       name: "McFishums",
       species: "Purplus Fishicus",
       food: "Lesser Fish",
       size: "Long Enough",
-      location: "MS Paint"
+      location: "MS Paint",
+      careTip1:
+        "Do not let it get too close to other fish, as it will devour them whole.",
+      careTip2: "Preferred Water Temp: Slightly Chilly.",
     },
 
     {
-        id: 3,
-        name: "JoeFish",
-        species: "Scalium Fishicus",
-        food: "Warmed Up Popcorn",
-        size: "Just Right",
-        location: "My Other Island",
+      id: 3,
+      image: "images/joeFish.jpg",
+      altText: "A green fish with a dorsal fin",
+      name: "JoeFish",
+      species: "Scalium Fishicus",
+      food: "Warmed Up Popcorn",
+      size: "Just Right",
+      location: "My Other Island",
+      careTip1: "Do Not Navigate With Your Mouse Near This Fish!",
+      careTip2: "Allergic to Miro",
     },
 
     {
-        id: 4,
-        name: "Charlie",
-        species: "Pink Fish",
-        food: "TetraMin Tropical Flakes",
-        size: "Chonk",
-        location: "My Island"
-    }
+      id: 4,
+      image: "images/pinkFish.jpg",
+      altText: "A large pink fish with a brown newsboy cap",
+      name: "Charlie",
+      species: "Pink Fish",
+      food: "TetraMin Tropical Flakes",
+      size: "Chonk",
+      location: "My Island",
+      careTip1: "Prefers small enclosed spaces.",
+      careTip2:
+        "The hat must be cleaned regularly. Care should be taken when removing it as the fish is extremely territorial.",
+    },
   ],
 };
 
 //allows us to export this database to other js files
 export const getFish = () => {
-    return database.fish.map(fish => ({...fish}))
+  return database.fish.map((fish) => ({ ...fish }));
 };
-
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
 //bring in the get fish function
-import { getFish } from './database.js';
+import { getFish } from "./database.js";
 
 //use the get fish function to store the array from the database into the const allFish here
 const allFish = getFish();
 
 for (const fish of allFish) {
-    console.log(fish)
+  console.log(fish);
 };
+
+//bring in the fishlist function now
+import { FishList } from "./fishList.js";
+
+const parentHTMLElement = document.querySelector(".fishList");
+
+parentHTMLElement.innerHTML = FishList();
